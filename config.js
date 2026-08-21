2199d5218dae02a52aaaffc0f02586bcb1ca6260
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-21 09:14:32 -0300

    Update version and API version in config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = Object.freeze({
   FIELD: "Missão Oeste do Pará",
   UNION: "União Norte Brasileira",
   DIVISION: "Divisão Sul-Americana",
-  VERSION: "2.2.5",
-  API_VERSION: "2.5"
+  VERSION: "2.2.6",
+  API_VERSION: "2.6"
 });
