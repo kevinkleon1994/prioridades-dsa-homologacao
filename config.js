66a360f515f42416134c8e6b5e550c8740db308d
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-21 15:23:06 -0300

    Update version and API version to 2.2.8

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = Object.freeze({
   FIELD: "Missão Oeste do Pará",
   UNION: "União Norte Brasileira",
   DIVISION: "Divisão Sul-Americana",
-  VERSION: "2.2.7",
-  API_VERSION: "2.7"
+  VERSION: "2.2.8",
+  API_VERSION: "2.2.8"
 });
