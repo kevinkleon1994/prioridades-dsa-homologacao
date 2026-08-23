c0d0ba470b25790719069081169a9f868fecff33
Kevin Fernandes <kevinkleon1994@gmail.com>
2026-08-23 02:29:43 -0300

    Update version from 2.2.12 to 2.2.13

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = Object.freeze({
   FIELD: "Missão Oeste do Pará",
   UNION: "União Norte Brasileira",
   DIVISION: "Divisão Sul-Americana",
-  VERSION: "2.2.12",
+  VERSION: "2.2.13",
   API_VERSION: "2.5"
 });
